31e3f201f96e3fbccbe65b7ba0125c3c65553520
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-01 23:37:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'John Doe', email: 'afafjkh@gmail.com' },
+    { id: 2, name: 'Jane Doe', email: 'fafajkm@gmaol.com' },
+    { id: 3, name: 'John Smith', email: 'fahfklh@gmail.com' },
+    { id: 4, name: 'Jane Smith', email: 'dasfjkjah@gmail.com' }
+];
+
+// username: anikUser1
+// password: ZaAhCKOjxoNdHsP9
+
+
+
+const uri = "mongodb+srv://anikUser1:ZaAhCKOjxoNdHsP9@cluster0.dwwrr0y.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db("simpleNode").collection("users");
+        const user = { name: "John Doe gsdg", email: "fafafayfdfbfasdf@gmail.com" };
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            user.id = result.insertedId;
+            res.send(user);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Simple node server running');
+});
+
+app.get('/users', (req, res) => {
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const filtered = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(filtered);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
